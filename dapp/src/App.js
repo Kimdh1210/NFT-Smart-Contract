7b10267ba6df6d1b24032342709c3c7d6318a8a1
김동현 <kimdh@gimdonghyeon-ui-MacBookAir.local>
2024-04-17 16:15:40 +0900

    Connect MetaMask

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,8 +1,48 @@
-import {Box, Button} from "@chakra-ui/react"
+import {Box, Button, Center} from "@chakra-ui/react"
+import {useState} from "react";
 
 const App = () => {
+
+  const [account, setAccount] = useState("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      } else {
+        alert("Install MetaMask!!!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logOut = () => {
+    setAccount("");
+  };
+
   return (
-    <Box>Hello, React!<Button colorScheme="blue" ml={8}>BTN</Button></Box>
+    <Box minH="100vh">
+      <Box bgColor="yellow.100" px={4} py={8} textAlign="center">
+        {account ? (
+          <Box fontWeight="bold">
+            Hello, {account.substring(0, 4)}...
+            {account.substring(account.length - 4)}
+            <Button ml={4} colorScheme="yellow" onClick={logOut}>
+              LogOut
+            </Button>
+          </Box>
+        ) : (
+          <Button colorScheme="yellow" onClick={getAccount}>
+            MetaMask
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 }
 
